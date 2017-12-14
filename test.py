34929562323a1f0372e852370f6dcb13887ad433
thomas clavier <thomas.clavier70@yahoo.fr>
2017-12-14 14:32:03 +0100

    Initial commit

---
+++ test.py
@@ -0,0 +1,8 @@
+annee = input("Saisissez une année: ")
+
+annee = int(annee)
+
+if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 !=0):
+    print("L'année saisie est bissextile.")
+else:
+    print("L'année saisie n'est pas bissextile.")
